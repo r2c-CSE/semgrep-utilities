98615538953fc2a44663b925b24d324d39fa6279
sebasrevuelta <sebas@r2c.dev>
2023-06-30 16:50:35 +0200

    add new parameters

--- import_semgrep_to_defect_dojo.py
+++ import_semgrep_to_defect_dojo.py
@@ -2,19 +2,18 @@ import requests
 import sys
 import os
 
-URL_DEFECT_DOJO = "http://localhost:8080"   
 
-def uploadToDefectDojo(filename, is_new_import, token):
+def uploadToDefectDojo(is_new_import, token, url, product_name, filename, engagement_name):
     multipart_form_data = {
         'file': (filename, open(filename, 'rb')),
         'scan_type': (None, 'Semgrep JSON Report'),
-        'product_name': (None, 'chess-game'),
-        'engagement_name': (None, 'semgrep'),
+        'product_name': (None, product_name),
+        'engagement_name': (None, engagement_name),
     }
 
-    uri = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
+    endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
     r = requests.post(
-        URL_DEFECT_DOJO + uri,
+        url + endpoint,
         files=multipart_form_data,
         headers={
             'Authorization': 'Token ' + token,
@@ -32,4 +31,14 @@ if __name__ == "__main__":
     except KeyError: 
         print("Please set the environment variable DEFECT_DOJO_API_TOKEN") 
         sys.exit(1)
-    uploadToDefectDojo("report.json", False, token)
+    if len(sys.argv) == 9:
+        url = sys.argv[2]
+        product_name = sys.argv[4]
+        engagement_name = sys.argv[6]
+        report = sys.argv[8]
+        uploadToDefectDojo(False, token, url, product_name, report, engagement_name)
+    else:
+        print(
+            'Usage: python3 import_semgrep_to_defect_dojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE')
+    
+    
