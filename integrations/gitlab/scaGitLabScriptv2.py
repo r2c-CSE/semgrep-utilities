1582b32f67dd130be9153355386fe3881129f5dc
sudor2c-cse <sudor2cexamples@gmail.com>
2023-07-24 20:49:18 +0200

    add new version

--- integrations/gitlab/scaGitLabScriptv2.py
+++ integrations/gitlab/scaGitLabScriptv2.py
@@ -7,11 +7,14 @@ def conversion_semgrep_to_gitlab(report_semgrep, data):
     with open(report_semgrep, 'r') as file_semgrep:
         data_semgrep = json.load(file_semgrep)
         for vuln in data_semgrep['results']:
-            new_vuln = {"id": vuln.get('extra')['fingerprint'],
+            new_vuln = {
+                        ## "id": vuln.get('extra')['fingerprint'],
+                        "id": "0e8294b7f5226e2e75d8369e100cd610face57ef67bc1af625ddb250785726a7",
                         "name": vuln['check_id'],
                         "description": vuln.get('extra')['message'],
                         "cve": vuln.get('extra').get('metadata')['sca-vuln-database-identifier'],
-                        "severity": to_hungarian_case(vuln.get('extra').get('metadata')['sca-severity']),
+                        ##"severity": to_hungarian_case(vuln.get('extra').get('metadata')['sca-severity']),
+                        "severity": "Low",
                         "solution": "Upgrade to version 2.8.2 or above", ## TODO: get all solutions 
                         "location": {
                             "file": "pom.xml",
