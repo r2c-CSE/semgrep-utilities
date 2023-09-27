842db17046bbc45c4f5c8800698be8f9075635e1
David Whitlow <david.whitlow@r2c.dev>
2023-09-26 18:19:28 -0700

    update to dedup with org members and write an output file

---
+++ utilities/contributors/README.md
@@ -0,0 +1,47 @@
+# github_recent_contributors.py
+This script is meant to help estimate the number of contributors that are active within a Github organization over a period of time.
+
+The script does not use exactly the same logic as Semgrep in determining active contributors but should be helpful in determining a rough estimate.
+
+## Usage
+You'll need to first export a [Github PAT](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens) into your environment for the script to use.
+
+Export your PAT as the variable `GITHUB_PERSONAL_ACCESS_TOKEN`.  Example:
+```export GITHUB_PERSONAL_ACCESS_TOKEN=ghp_BunchOfSecretStuffGoesHere```
+
+The Token will need the following scopes:
+- repo
+- read:org
+- read:user
+- user:email
+
+The script takes the following arguements:
+- The name of the github organization
+- The number of days to look over (we recommend 90 as a safe default)
+- An output filename to store the details from the execution
+
+## Output
+Example console output:
+```
+Total commit authors in the last 90 days: 33
+Total members in r2c-cse: 16
+Total unique contributors from r2c-cse in the last 90 days: 5
+```
+
+Example output file:
+```
+{
+    "organization": "r2c-cse",
+    "date": "2023-09-26",
+    "number_of_days_history": 90,
+    "org_members": [
+        ...
+    ],
+    "commit_authors": [
+        ...
+    ],
+    "commiting_members": [
+        ...
+    ]
+}
+```
--- utilities/contributors/github_recent_contributors.py
+++ utilities/contributors/github_recent_contributors.py
@@ -32,25 +32,50 @@ import requests
 import os
 from datetime import datetime, timedelta
 import argparse
+import json
 
-def get_contributors(org_name, number_of_days):
-    token = os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN")
-    if not token:
-        raise ValueError("Please set your GITHUB_PERSONAL_ACCESS_TOKEN as an environment variable.")
-    headers = {'Authorization': f'token {token}'}
-    
-    # Fetch all repositories in the organization
+
+def get_repos(org_name, headers):
+    """Fetch all repositories for the given organization."""
     response = requests.get(
         f'https://api.github.com/orgs/{org_name}/repos',
         headers=headers
     )
-    repos = response.json()
+    
+    if response.status_code != 200:
+        raise ValueError(f"Error fetching repositories for organization {org_name}. Status code: {response.status_code}")
+
+    return response.json()
+
+def get_organization_members(org_name, headers):
+    """Fetch all members of the organization."""
+    members = []
+    page = 1
+    while True:
+        response = requests.get(
+            f'https://api.github.com/orgs/{org_name}/members?page={page}',
+            headers=headers
+        )
+        if response.status_code != 200:
+            break
+        members_page = response.json()
+        if not members_page:
+            break
+        members.extend(members_page)
+        page += 1
+    return {member['login'] for member in members}
+
+def get_contributors(org_name, number_of_days, headers):
+    # init contributor set
+    unique_contributors = set()
+    unique_authors = set()
+    
+    # Fetch all repositories in the organization
+    repos = get_repos(org_name, headers)
 
     # Date range calculation
     since_date = (datetime.utcnow() - timedelta(days=number_of_days)).isoformat() + "Z"
     until_date = datetime.utcnow().isoformat() + "Z"
-
-    unique_contributors = set()
 
     # Loop through each repository in the organization
     for repo in repos:
@@ -65,21 +90,52 @@ def get_contributors(org_name, number_of_days):
         )
         
         commits = response.json()
+
         if isinstance(commits, list):
             for commit in commits:
                 unique_contributors.add(commit['commit']['author']['name'])
+                if commit['author']:
+                    unique_authors.add(commit['author']['login'])
         else:
             print(f"Repo: {repo_name} is empty.") 
         
-    # Print unique contributors and their total count        
-    print(f"Unique contributors in the last {number_of_days} days:", unique_contributors)
-    print(f"Total number of unique contributors in the last {number_of_days} days:", len(unique_contributors))
+    return unique_contributors, unique_authors
 
+def report_contributors(org_name, number_of_days, output_file):
+    # init github auth
+    token = os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN")
+    if not token:
+        raise ValueError("Please set your GITHUB_PERSONAL_ACCESS_TOKEN as an environment variable.")
+    headers = {'Authorization': f'token {token}'}
+
+    org_members = get_organization_members(org_name, headers)
+    unique_contributors, unique_authors = get_contributors(org_name, number_of_days, headers)
+    
+    if output_file:
+        output_data = {
+            "organization": org_name,
+            "date": datetime.today().date().strftime('%Y-%m-%d'),
+            "number_of_days_history": number_of_days,
+            "org_members": list(org_members),
+            "commit_authors": list(unique_authors),
+            "commiting_members": list(unique_authors & org_members)
+        }
+
+        with open(output_file, 'w') as file:
+            json.dump(output_data, file)
+
+    # Print unique contributors and their total count        
+    print(f"Total commit authors in the last {number_of_days} days:", len(unique_authors))
+    print(f"Total members in {org_name}:", len(org_members))
+    print(f"Total unique contributors from {org_name} in the last {number_of_days} days:", len(unique_authors & org_members))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Get unique contributors from a GitHub organization.")
     parser.add_argument("org_name", help="The name of the GitHub organization.")
     parser.add_argument("number_of_days", type=int, help="Number of days to look over.")
+    parser.add_argument("output_filename", help="A file to log output.")
     
     args = parser.parse_args()
-    get_contributors(args.org_name, args.number_of_days)
+    report_contributors(args.org_name, args.number_of_days, args.output_filename)
+
+
