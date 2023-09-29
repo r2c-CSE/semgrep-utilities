67db2953807ecc4aa70eb4258ff6412bd4d1434f
Alexis Grant <alexisg@gmail.com>
2023-09-29 16:44:33 -0700

    Improve error handling

--- semgrep-ci/github/onboard-repos.sh
+++ semgrep-ci/github/onboard-repos.sh
@@ -1,7 +1,7 @@
 #!/opt/homebrew/bin/bash
 
 # TODO: Work out a way to change the cron per-repo (probably not critical but wd be better)
-# Works best if semgrep.yml file uses org secret or reusable workflow (avoids needing to add secret everywhere)
+# Where does the secret come from? Maybe we should do the reusable workflows?
 
 if [[ -z $1 ]]
 then
@@ -19,7 +19,7 @@ fi
 GH_ORG_NAME=$1
 PR_TITLE="Add semgrep.yml to .github/workflows to run Semgrep scans"
 PR_BODY="Adding Semgrep GitHub action to scan code for security issues"
-REPO_LIMIT=200
+REPO_LIMIT=5
 BRANCH_NAME="add-semgrep"
 SEMGREP_YML_BASE64=$(cat semgrep.yml | base64)
 
@@ -36,22 +36,48 @@ GITHUB_BRANCHES=($(jq -r '.[].defaultBranchRef.name' repos.json))
 for i in ${!GITHUB_REPOS[@]}; do
   GH_REPO=${GITHUB_REPOS[$i]}
   GH_DEFAULT_BRANCH=${GITHUB_BRANCHES[$i]}
+  # Check if the file exists on the default branch
+  SEMGREP_ONBOARDED=`gh api repos/{owner}/{repo}/contents/.github/workflows/semgrep.yml\?branch=$GH_DEFAULT_BRANCH --jq '.name'`
+  if [[ $SEMGREP_ONBOARDED == 'semgrep.yml' ]]
+  then
+    echo "Semgrep already onboarded for $GH_REPO. Moving to next repo."
+    continue
+  fi
   echo "Creating $BRANCH_NAME on $GH_REPO based on $GH_DEFAULT_BRANCH"
   # Get the SHA of the default branch
   DEFAULT_BRANCH_SHA=$(gh api repos/{owner}/{repo}/git/ref/heads/$GH_DEFAULT_BRANCH --jq ".object.sha")
   # Create the new branch off the default branch sha
-  gh api repos/{owner}/{repo}/git/refs -X POST -F ref=refs/heads/$BRANCH_NAME -F sha=$DEFAULT_BRANCH_SHA
-  # TODO: Check return and flag if ref already exists (probably OK) or some other issue (probably not OK)
+  BRANCH_CREATION_RESULT=`gh api repos/{owner}/{repo}/git/refs -X POST -F ref=refs/heads/$BRANCH_NAME -F sha=$DEFAULT_BRANCH_SHA`
+  BC_STATUS=$?
+  if [[ $BRANCH_CREATION_RESULT =~ "Reference already exists" ]]
+  then
+    echo "A branch with the name $BRANCH_NAME already exists on $GH_REPO, no need to create one."
+  elif [[ $BC_STATUS -ne 0 ]]
+  then
+    echo "Something else went wrong for $GH_REPO: $BRANCH_CREATION_RESULT"
+    continue    
+  fi
   # Add the file to the branch (creates a commit also)
   echo "Creating semgrep.yml on $BRANCH_NAME in $GH_REPO"
-  gh api repos/{owner}/{repo}/contents/.github/workflows/semgrep.yml \
+  FILE_CREATION_RESULT=`gh api repos/{owner}/{repo}/contents/.github/workflows/semgrep.yml \
     -X PUT \
     -H "Accept: application/vnd.github.v3+json" \
     -F message="Add semgrep.yml" \
     -F content=$SEMGREP_YML_BASE64 \
-    -F branch=$BRANCH_NAME
-  # TODO: Check return and flag if "sha wasn't supplied" (file is already present, probably OK) or something else (probably not OK)
+    -F branch=$BRANCH_NAME`
+  FC_STATUS=$?
+  if [[ $FILE_CREATION_RESULT =~ "\\\"sha\\\" wasn't supplied" ]]
+  then
+    echo "A semgrep.yml file already exists on $BRANCH_NAME in $GH_REPO, no need to create one."
+  elif [[ $FC_STATUS -ne 0 ]]
+  then
+    echo "Something else went wrong for $GH_REPO: $FILE_CREATION_RESULT"
+    continue    
+  fi
   # Create a PR with the branch and added file
   echo "Creating PR for $BRANCH_NAME in $GH_REPO to $GH_DEFAULT_BRANCH"
   gh pr create -b "$PR_BODY" -R $GH_REPO -t "$PR_TITLE" --base $GH_DEFAULT_BRANCH -H $BRANCH_NAME
+  
+  # Cleanup
+  rm -f repos.json
 done
