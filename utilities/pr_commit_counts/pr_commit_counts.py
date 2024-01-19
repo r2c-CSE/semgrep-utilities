d2a7e5c655068d84b122fc2a04b4e2ebcfdff298
Nitin Nayar <119853191+nnayar-r2c@users.noreply.github.com>
2024-01-19 11:46:15 +0000

    Update pr_commit_counts.py

--- utilities/pr_commit_counts/pr_commit_counts.py
+++ utilities/pr_commit_counts/pr_commit_counts.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime, timedelta
 
 def get_paginated_results(url, headers):
     """ Function to handle pagination for GitHub API requests """
@@ -16,27 +17,28 @@ def get_paginated_results(url, headers):
             raise Exception(f"GitHub API request failed with status code {response.status_code}")
     return results
 
-def get_repos(organization, headers):
+def get_repos(BASE_URL, organization, headers):
     """ Get a list of repositories for a given organization """
-    url = f"https://api.github.com/orgs/{organization}/repos"
+    url = f"{BASE_URL}/orgs/{organization}/repos"
     return get_paginated_results(url, headers)
 
-def get_pull_requests(organization, repo, headers):
+def get_pull_requests(BASE_URL, organization, repo, headers):
     """ Get a list of pull requests for a given repository """
-    url = f"https://api.github.com/repos/{organization}/{repo}/pulls?state=all"
+    url = f"{BASE_URL}/repos/{organization}/{repo}/pulls?state=all"
     return get_paginated_results(url, headers)
 
-def get_commits_for_pull_request(organization, repo, pull_number, headers):
+def get_commits_for_pull_request(BASE_URL, organization, repo, pull_number, headers):
     """ Get a list of commits for a given pull request """
-    url = f"https://api.github.com/repos/{organization}/{repo}/pulls/{pull_number}/commits"
+    url = f"{BASE_URL}/repos/{organization}/{repo}/pulls/{pull_number}/commits"
     return get_paginated_results(url, headers)
 
 def main():
-    # organization = input("Enter the organization name: ")
-    # github_token = input("Enter your GitHub access token: ")
-
-    organization = "ORGANIZATION_NAME" # Replace with your organization name
-    github_token = os.getenv('SEMGREP_API_WEB_TOKEN') # Add your GH token as a environment variable
+    organization = os.getenv("GITHUB_ORG")
+    if organization is None:
+        organization = input("Enter the organization name: ")
+    
+    github_token = os.getenv('GITHUB_TOKEN') # "" # Replace with your GitHub token
+    BASE_URL = "https://api.github.com"
 
     headers = {
         'Authorization': f'token {github_token}',
@@ -46,18 +48,19 @@ def main():
     total_commits = 0
     total_prs = 0
 
-    repos = get_repos(organization, headers)
+    repos = get_repos(BASE_URL, organization, headers)
 
     for repo in repos:
-        pull_requests = get_pull_requests(organization, repo['name'], headers)
+        repo_name = repo['name']
+        pull_requests = get_pull_requests(BASE_URL, organization, repo_name, headers)
         for pr in pull_requests:
-            commits = get_commits_for_pull_request(organization, repo['name'], pr['number'], headers)
-            print(f'total number of commits in repo: {repo["name"]}, pr-#{pr["number"]}, pr-title: {pr["title"][:32]}, head: {pr["head"]["ref"][:16]} into base: {pr["base"]["ref"][:16]}  - {len(commits)}')
-            total_prs += 1
-            total_commits += len(commits)
-    
-    # print(f"Total number of commits in pull requests: {total_commits}")
+            commits = get_commits_for_pull_request(BASE_URL, organization, repo_name, pr['number'], headers)
+            one_year_ago = datetime.now() - timedelta(days=365)
+            commits_in_pr_repo_last_year = sum(1 for commit in commits if datetime.strptime(commit['commit']['author']['date'], '%Y-%m-%dT%H:%M:%SZ') > one_year_ago)
 
+            print(f'total number of commits in repo: {repo_name}, pr-#{pr["number"]}, pr-title: {pr["title"][:32]}, head: {pr["head"]["ref"][:16]} into base: {pr["base"]["ref"][:16]}  - {commits_in_pr_repo_last_year} / {len(commits)}')
+            total_prs += 1
+            total_commits += commits_in_pr_repo_last_year
 
     annual_pr_events_count, annual_pr_count = total_commits, total_prs
     avg_monthly_pr_events_count = (int(100*annual_pr_events_count/12))/100
