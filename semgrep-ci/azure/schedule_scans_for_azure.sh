71c19ee2100e0d2adab86a91ffe8cba37fb012be
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2024-02-21 17:39:06 +0100

    Update schedule_scans_for_azure.sh

--- semgrep-ci/azure/schedule_scans_for_azure.sh
+++ semgrep-ci/azure/schedule_scans_for_azure.sh
@@ -6,13 +6,11 @@
 ## AZURE_ORGANIZATION
 ## AZURE_PROJECT
 
-
-echo 'https://dev.azure.com/'${AZURE_ORGANIZATION}'/'${AZURE_PROJECT}'/_apis/git/repositories'
-repo_name_list=$(curl --request GET --url 'https://dev.azure.com/'${AZURE_ORGANIZATION}'/'${AZURE_PROJECT}'/_apis/git/repositories' --header 'Authorization: Bearer '${AZURE_TOKEN} --header 'Accept: application/json' | jq -r '.value[].name')
+repo_name_list=$(curl -H "Authorization: Basic $(echo -n ":${AZURE_TOKEN}" | base64)" --request GET --url 'https://dev.azure.com/'${AZURE_ORGANIZATION}'/'${AZURE_PROJECT}'/_apis/git/repositories' | jq -r '.value[].name')
 for repo_name in ${repo_name_list}; do
     echo "Scanning repo: ${repo_name}"
     rm -rf ${repo_name}
-    git clone https://dev.azure.com/${AZURE_ORGANIZATION}/${AZURE_PROJECT}/_git/${repo_name}
+    git clone https://${AZURE_TOKEN}@dev.azure.com/${AZURE_ORGANIZATION}/${AZURE_PROJECT}/_git/${repo_name}
     cd ${repo_name}
     docker run -e SEMGREP_APP_TOKEN=$SEMGREP_APP_TOKEN  -e SEMGREP_REPO_NAME=${repo_name} -v "$(pwd):$(pwd)" --workdir $(pwd) returntocorp/semgrep semgrep ci || true
     cd ..
